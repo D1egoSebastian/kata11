095da5f6429a67b0dcfc52d2c07f4460b243d7f3
Diego Sebastian <diegosrr24@gmail.com>
2024-03-19 21:09:58 -0400

    try #1

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-print("this is a test from pycharm")
 
-def sum(a, b):
-    return a + b
 
-print(sum(4,5))
+def compare_chars(char1, char2):
+    if char1 < char2:
+        return -1
+    elif char1 > char2:
+        return 1
+
+def sort_it(string):
+
+    stringlower = string.lower()
+
+    not_special = ""
+    for char in stringlower:
+        if char.isalpha():
+            not_special += char
+
+    is_sorted = False
+    while not is_sorted:
+        is_sorted = True
+        for i in range(len(not_special) - 1):
+            char1 = not_special[i]
+            char2 = not_special[i + 1]
+            compare = compare_chars(char1, char2)
+
+            if compare == 1:
+                not_special = not_special[:i] + char2 + char1 + not_special[i + 2:]
+                is_sorted = False
+
+    return not_special
+
+string = "ABC"
+answer = sort_it(string)
+print(answer)
+
+
---
+++ test_main.py
@@ -0,0 +1,33 @@
+import unittest
+from main import sort_it
+
+
+
+class testSortIt(unittest.TestCase):
+    def test_sort1(self):
+        string = "A"
+        expected = "a"
+        self.assertEqual(sort_it(string), expected)
+
+    def test_sort2(self):
+        string = "A!@#"
+        expected = "a"
+        self.assertEqual(sort_it(string), expected)
+
+    def test_sort3(self):
+        string = "When not studying nuclear physics, Bambi likes to play beach volleyball"
+        expected = "aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy"
+        self.assertEqual(sort_it(string), expected)
+
+    def test_sort4(self):
+        string = ""
+        expected = ""
+        self.assertEqual(sort_it(string), expected)
+
+    def test_sort5(self):
+        string = "bEaCh"
+        expected = "abceh"
+
+if __name__ == '__main__':
+    unittest.main()
+
